8aae4cd61a7b146f12e00d908a57d3e9b1cda8ba
JeanCarloEM <jeancarlo@jeancarloem.com>
2025-11-24 18:05:48 -0300

    removido /radio/

--- radio/assets/js/repetidoras/modelos/uv5rh.cjs
+++
@@ -1,1 +0,0 @@
-const isNODE="undefined"==typeof window,commom=isNODE?require(`../../../../../@assets/js/common${isNODE?".main":""}.cjs`):null,MODEL=["Location","Name","Frequency","Duplex","Offset","Tone","rToneFreq","cToneFreq","DtcsCode","DtcsPolarity","RxDtcsCode","CrossMode","Mode","TStep","Skip","Power","Comment","URCALL","RPT1CALL","RPT2CALL","DVCODE"];
